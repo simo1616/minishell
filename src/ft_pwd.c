93a9a36bfcd7b1196cea8b11c72a3d28a407188e
ldsr18 <jordandecarroux@hotmail.com>
2025-02-04 10:53:11 +0100

    Ajout premiers builtins

---
+++ src/ft_pwd.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pwd.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdecarro <jdecarro@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/31 14:24:10 by jdecarro          #+#    #+#             */
+/*   Updated: 2025/01/31 14:24:10 by jdecarro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	ft_pwd(void)
+{
+	char	cwd[PATH_MAX];
+
+	if (getcwd(cwd, sizeof(cwd)))
+	{
+		printf("%s\n", cwd);
+		return (0);
+	}
+	else
+	{
+		perror("pwd");
+		return (1);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	ft_pwd();
+	return (0);
+}
