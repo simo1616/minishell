002e02af8a15475d5b04e6be65c9acd7b7a6a2a4
ldsr18 <jordandecarroux@hotmail.com>
2025-02-07 17:58:37 +0100

    Correction export

--- src/ft_export.c
+++ src/ft_export.c
@@ -12,6 +12,43 @@
 
 #include "minishell.h"
 
+static void	ft_sort_env(char **env)
+{
+	int		i;
+	int		j;
+	char	*tmp;
+
+	i = 0;
+	while (env[i])
+	{
+		j = i + 1;
+		while (env[j])
+		{
+			if (ft_strcmp(env[i], env[j]) > 0)
+			{
+				tmp = env[i];
+				env[i] = env[j];
+				env[j] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+static void	ft_print_sorted_env(char **env)
+{
+	int	i;
+
+	ft_sort_env(env);
+	i = 0;
+	while (env[i])
+	{
+		ft_printf("declare -x %s\n", env[i]);
+		i++;
+	}
+}
+
 int	ft_export(char **args, t_shell_env *shell_env)
 {
 	int		i;
@@ -19,6 +56,11 @@ int	ft_export(char **args, t_shell_env *shell_env)
 
 	if (!args || !args[0] || !shell_env)
 		return (1);
+	if (!args[1])
+	{
+		ft_print_sorted_env(shell_env->env);
+		return (0);
+	}
 	if (!ft_strchr(args[1], '=') || args[1][0] == '=')
 	{
 		ft_printf("-minishell: export:'%s': not a valid identifier\n", args[1]);
@@ -43,6 +85,3 @@ int	ft_export(char **args, t_shell_env *shell_env)
 	shell_env->env = new_env;
 	return (0);
 }
-
-// qjsdhflkqsjhdf
-
